d3d872d072133177b38cf627815d5cb508efa9ae
this-surya <uyahasibuan.123@gmail.com>
2026-06-19 20:16:01 +0700

    add:analitic

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased bg-[#FFFFFF] scroll-smooth`}
     >
+      <Analytics/>
       <body className="min-h-full flex flex-col scroll-smooth">{children}</body>
     </html>
   );
